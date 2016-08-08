6bcdc479d7b435852cb45b2d53fc40c09f994047
Steven Gloyd <jemimacakes@gmail.com>
2016-08-07 18:06:57 -0700

    Modified LED options

--- src/RGB_Button_Arduino.ino
+++ src/RGB_Button_Arduino.ino
@@ -147,7 +147,7 @@ Outputs: none.
 Description: Standard Arduino loop function. Loops indefinitely.
 *****************************************************************/
 void loop(){
-	flow(5, 0, 200, 195, "right");
+	flow(5, 0, 250, 273, "right");
 
 	#ifdef DEBUG
 		while(1){};
